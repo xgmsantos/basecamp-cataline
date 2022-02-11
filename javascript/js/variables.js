87f35b3dfb2332cf0457fbc1f75207e9e38400cb
Gabriel M. dos Santos <xgmsantos@gmail.com>
2022-02-11 16:27:42 -0500

    Variables class

---
+++ javascript/js/variables.js
@@ -0,0 +1,16 @@
+var name = 'Gabriel Maciel' // string
+var age = 21 // number
+var weight = 95.0 // float
+var alive = true // boolean
+var food = null // null
+var time // undefined
+
+var fruits = ['Morango', 'Maçã', 'Abacate'] // array
+
+var person = {
+    name: 'Gabriel Maciel',
+    age: 21,
+    weight: 95.0
+} // object
+
+console.log(person.name)
