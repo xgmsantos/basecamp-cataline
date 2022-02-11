5115557608e5c82046acbb79c47e135705842725
Gabriel M. dos Santos <xgmsantos@gmail.com>
2022-02-11 16:31:11 -0500

    Concatenetes class

---
+++ javascript/js/concatenate.js
@@ -0,0 +1,7 @@
+var product = 'camisa'
+var price = 10
+
+// Pode concatenar da seguinte maneira 
+console.log('O preço de ' + product + ' é R$ ' + price)
+// Ou usar template strings
+console.log(`O preço de ${product} é R$ ${price}`)
